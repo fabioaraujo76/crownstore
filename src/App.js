eda3eed62b01b0de82b534d3a783483a9e6f3da8
FABIO DE ARAUJO <fabiodearaujo@FABIOs-MacBook-Air.local>
2021-05-06 15:58:08 +0100

    included Routing to the project and tested with Hats and Jackets

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <HomePage />
-    );
-  }
+const HatsPage = () => (
+  <div>
+    <h1>hats page</h1>
+  </div>
+);
+
+const JacketsPage = () => (
+  <div>
+    <h1>Jackets page</h1>
+  </div>
+);
+
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop/hats' component={HatsPage} />
+        <Route path='/shop/jackets' component={JacketsPage} />
+      </Switch>
+    </div>
+  );
 }
+
 
 export default App;
