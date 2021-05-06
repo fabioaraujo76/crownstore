823f064d90b4ca8ca1fcfba0c27b5eb4055dd6cf
FABIO DE ARAUJO <fabiodearaujo@FABIOs-MacBook-Air.local>
2021-05-06 10:36:08 +0100

    created homepage component and included Sass support

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
+import HomePage from './components/homepage/homepage.component';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       Hello World!
-      </div>
+      <HomePage />
     );
   }
 }
