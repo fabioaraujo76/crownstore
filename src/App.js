1aafee2f0336cd76653a793ac9510956f568818d
FABIO DE ARAUJO <fabiodearaujo@FABIOs-MacBook-Air.local>
2021-05-10 22:20:28 +0100

    created Sign in component, form and custom button components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Header from './components/header/header.component'
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component'
+import ShopPage from './pages/shop/shop.component';
+import SignInSignUpPage from './pages/signin-signup/signin-signup.component';
+
 import './App.css';
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.component'
+import './signin-signup.style.scss'
+
+const SignInSignUpPage = () => (
+    <div className='signin-signup'>
+    <SignIn/> 
+    </div>
+);
+    
+
+export default SignInSignUpPage;
